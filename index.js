5fc6a445b7fc8c75bb6976804f78254da4c7292c
Paul Melton <melton.paul@hotmail.com>
2022-04-25 18:22:16 -0500

    Built localstorage personal watchlist.

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 const search = document.getElementById("searchBar")
 const movieContainer = document.getElementById("movieContainer")
-let searchArray = []
+let movieAdd = document.getElementsByClassName("watchlist-add")
+let savedId = []
 
 
 document.getElementById("searchBtn").addEventListener("click", function(){
     let searchValue = search.value
     fetch(`http://www.omdbapi.com/?apikey=9980ac75&s=${searchValue}`)
         .then(res => res.json())
-        .then(data => {console.log(data.Search)
+        .then(data => {
             data.Search.forEach(movie => {
+                noData.style.display = "none"
                 fetch(`http://www.omdbapi.com/?apikey=9980ac75&t=${movie.Title}`)
                     .then(res => res.json())
-                    .then(data => {console.log(data)
-                        const {Title, Runtime, Genre, Plot, imdbRating, Poster} =  data
-                        movieContainer.innerHTML += `
-                            <div class="movie-container container">
-                                <img class="movie-img" src="${Poster}" alt="A poster of the movie ***">
-                                <div class="movie-content">
-                                    <div class="movie-header">
-                                        <h2 class="movie-title clear__bottom">${Title}</h2>
-                                        <i class="fa fa-star clear__bottom"></i>
-                                        <p class="movie-rating clear__bottom">${imdbRating}</p>
-                                    </div>
-                                    <div class="movie-tags">
-                                        <p class="movie-tag movie-time clear__bottom">${Runtime}</p>
-                                        <p class="movie-tag movie-category clear__bottom">${Genre}</p>
-                                        <div class=" movie-tag watchlist-add">
-                                            <i class="fa fa-plus"></i>
-                                            <p class="clear__bottom">Add to Watchlist</p>
-                                        </div>
-                                    </div>
-                                    <div class="movie-desc">
-                                        <p>${Plot}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-                        searchArray.push(movieHtml)
-                    })
+                    .then(data => movieContainer.innerHTML += renderPage(data))
                 });
-                
-                console.log(searchArray)
-
-        
-        
-        
         })
-
-            });
+});
         
+function renderPage(data) {
+    const {Title, Runtime, Genre, Plot, imdbRating, imdbID, Poster} =  data
+    return  `
+        <div class="movie-container container">
+            <img class="movie-img" src="${Poster}" alt="A poster of the movie ***">
+            <div class="movie-content">
+                <div class="movie-header">
+                    <h2 class="movie-title clear__bottom">${Title}</h2>
+                    <i class="fa fa-star clear__bottom"></i>
+                    <p class="movie-rating clear__bottom">${imdbRating}</p>
+                </div>
+                <div class="movie-tags">
+                    <p class="movie-tag movie-time clear__bottom">${Runtime}</p>
+                    <p class="movie-tag movie-category clear__bottom">${Genre}</p>
+                    <div class=" movie-tag watchlist-add" onclick="addWatch('${imdbID}')">
+                        <i class="fa fa-plus"></i>
+                        <p class="clear__bottom">Add to Watchlist</p>
+                    </div>
+                </div>
+                <div class="movie-desc">
+                    <p>${Plot}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
 
+
+function addWatch(id){
+    if(savedId.includes(id)){
+            window.alert("You already have this movie in your watch list!")
+        }
+    else {
+            savedId.push(id)
+            window.localStorage.setItem("savedIds", JSON.stringify(savedId))
+        }
+    }
+    
+function pageLoad(){
+    if(window.localStorage.length > 0){
+        savedId = JSON.parse(window.localStorage.getItem("savedIds"))
+    }
+    
+}
+pageLoad()
---
+++ watchlist.js
@@ -0,0 +1,51 @@
+const stored = JSON.parse(window.localStorage.getItem("savedIds"))
+const noData = document.getElementById("noData")
+
+
+
+
+function renderList(){
+    if(stored.length > 0){
+    noData.style.display = "none"
+    stored.forEach(movieId => {
+        fetch(`http://www.omdbapi.com/?apikey=9980ac75&i=${movieId}&`)
+        .then(res => res.json())
+        .then(data => {
+            const {Title, Runtime, Genre, Plot, imdbRating, imdbID, Poster} =  data
+            movieContainer.innerHTML += `
+                    <div class="movie-container container">
+                        <img class="movie-img" src="${Poster}" alt="A poster of the movie ***">
+                        <div class="movie-content">
+                            <div class="movie-header">
+                                <h2 class="movie-title clear__bottom">${Title}</h2>
+                                <i class="fa fa-star clear__bottom"></i>
+                                <p class="movie-rating clear__bottom">${imdbRating}</p>
+                            </div>
+                            <div class="movie-tags">
+                                <p class="movie-tag movie-time clear__bottom">${Runtime}</p>
+                                <p class="movie-tag movie-category clear__bottom">${Genre}</p>
+                                <div class=" movie-tag watchlist-add" onclick="removeMovie('${imdbID}')">
+                                    <i class="fa fa-x"></i>
+                                    <p class="clear__bottom">Remove</p>
+                                </div>
+                            </div>
+                            <div class="movie-desc">
+                                <p>${Plot}</p>
+                            </div>
+                        </div>
+                        </div>
+                        `
+        })
+    });}
+    }
+    
+
+
+
+function removeMovie(id){
+   const location = stored.indexOf(id)
+   stored.splice(location, 1)
+   window.localStorage.setItem("savedIds", JSON.stringify(stored))
+   document.location.reload()
+}
+renderList()
